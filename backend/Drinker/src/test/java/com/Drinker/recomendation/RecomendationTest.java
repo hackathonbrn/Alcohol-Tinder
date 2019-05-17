ea75102d529715d594384abf16f6ddcc7bb9818f
user-penguin <kobzev96@gmail.com>
2019-05-18 00:27:30 +0700

    create class Graph

--- backend/Drinker/src/test/java/com/Drinker/recomendation/RecomendationTest.java
+++ backend/Drinker/src/test/java/com/Drinker/recomendation/RecomendationTest.java
@@ -1,4 +1,10 @@
 package com.Drinker.recomendation;
 
+import org.junit.Test;
+
 public class RecomendationTest {
+    @Test
+    public void getFirstCircleTest() {
+        Graph graph = new Graph();
+    }
 }
