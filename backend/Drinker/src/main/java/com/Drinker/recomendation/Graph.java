ff1c6b636734094ba788d08e525decee8dcb865e
user-penguin <kobzev96@gmail.com>
2019-05-18 12:18:10 +0700

    create finding drinking companion

--- backend/Drinker/src/main/java/com/Drinker/recomendation/Graph.java
+++ backend/Drinker/src/main/java/com/Drinker/recomendation/Graph.java
@@ -1,6 +1,7 @@
 package com.Drinker.recomendation;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Graph {
@@ -26,5 +27,36 @@ public class Graph {
             }
         }
         return result;
+    }
+
+    public boolean checkFirstStageInclude(ArrayList<Integer> firstStage, int targetNeighbour) {
+        for (int neighbour: firstStage) {
+            if (neighbour == targetNeighbour) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public HashMap<Integer, Integer> getRecomendation(int target) {
+        ArrayList<Integer> fiirstNeighbours = getFirstStageNeighbour(target);
+        // получен список первых соседей, теперь требуется выявить среди них кандидатов
+
+        // мутим мапу для фиксирования количества дринкеров у друзей
+        HashMap<Integer, Integer> candidate = new HashMap<Integer, Integer>();
+        for (int mainNeighbour: fiirstNeighbours) {
+            ArrayList<Integer> secondStage = getFirstStageNeighbour(mainNeighbour);
+            for (int secondNeighbour: secondStage) {
+                if (secondNeighbour == target || checkFirstStageInclude(fiirstNeighbours, secondNeighbour)) {
+                    continue;
+                }
+                if (candidate.get(secondNeighbour) == null) {
+                    candidate.put(secondNeighbour, 1);
+                } else {
+                    candidate.put(secondNeighbour, candidate.get(secondNeighbour) + 1);
+                }
+            }
+        }
+        return candidate;
     }
 }
--- backend/Drinker/src/test/java/com/Drinker/recomendation/RecomendationTest.java
+++ backend/Drinker/src/test/java/com/Drinker/recomendation/RecomendationTest.java
@@ -3,6 +3,7 @@ package com.Drinker.recomendation;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -27,5 +28,20 @@ public class RecomendationTest {
         actual.add(1);
         actual.add(5);
         assertArrayEquals(actual.toArray(), expected.toArray());
+    }
+
+    @Test
+    public void gettingRecomendationListTest() {
+        Graph graph = new Graph();
+        graph.addPare(1, 2); graph.addPare(2, 1);
+        graph.addPare(1, 3); graph.addPare(3, 1);
+        graph.addPare(1, 4); graph.addPare(4, 1);
+        graph.addPare(2, 3); graph.addPare(3, 2);
+        graph.addPare(2, 5); graph.addPare(5, 2);
+        graph.addPare(2, 6); graph.addPare(6, 2);
+        graph.addPare(4, 6); graph.addPare(6, 4);
+        graph.addPare(4, 7); graph.addPare(7, 4);
+        HashMap<Integer, Integer> recomendedPeople = graph.getRecomendation(1);
+        System.out.println(recomendedPeople.toString());
     }
 }
