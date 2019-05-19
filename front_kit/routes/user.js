c98a86ed8efe77ad54baba33333ccc7fd2eafc4a
Вячеслав Нагель <v9gel@mail.ru>
2019-05-19 16:34:43 +0700

    Вывод из переменных

--- front_kit/routes/user.js
+++ front_kit/routes/user.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('user', { title: 'Пользователи' });
+  res.render('user', { title: 'Пользователи', user: {firstName: 'Ап', age:1, photo: 'h', alcohol: ['Ром', 'Мартини'], interests: ['Ром', 'Мартини']} });
 });
 
 module.exports = router;
