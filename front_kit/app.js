09dfb09706506eae071b2e06f6fac0577a64f4b2
Вячеслав Нагель <v9gel@mail.ru>
2019-05-19 15:32:57 +0700

    Профиль пользователя

--- front_kit/app.js
+++ front_kit/app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var userRouter = require('./routes/user');
 var chatRouter = require('./routes/chat');
 
 var app = express();
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', userRouter);
 app.use('/chat', chatRouter);
 
 // catch 404 and forward to error handler
